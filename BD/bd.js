7b52e135a821afaa5629427f842400f4ebfbe506
CarlosAAS071 <carlosad54321@gmail.com>
2025-11-02 15:56:11 -0600

    Usar variable de entorno para MongoDB

--- BD/bd.js
+++ BD/bd.js
@@ -1,9 +1,8 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 async function conectarBD(){
   try{
-    const conexion = await mongoose.connect("mongodb+srv://carlosaas071:carlos050050050@cluster0.5eml0mc.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
-    // const conexion = mongoose.connect("mongodb://localhost:27017/backend1")
+    const conexion = await mongoose.connect(process.env.MONGO_URI)
     console.log("Conexión establecida con Mongo Atlas.")
   }
   catch(ex){
